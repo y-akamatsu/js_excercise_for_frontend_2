7fad98c9ba34b45fdcd33066f91bbeac18fdf1bb
y-akamatsu <ak1985tk@gmail.com>
2018-12-10 22:34:57 +0900

    エクササイズの実装

--- main.js
+++ main.js
@@ -3,15 +3,17 @@
  *   - textContentを使う
  *     - 「JSで変更した後のタイトル」というテキストをセットする
  */
-
-
+const titleElement = document.getElementById('title');
+titleElement.textContent = 'JSで変更した後のタイトル';
 /**
- * 課題2: p要素でid属性値が `descripion` のテキスト内容を変更する
+ * 課題2: p要素でid属性値が `descripsion` のテキスト内容を変更する
  *   - innerHTMLを使う
  *     - 「JSで変更した後の説明文」というテキストをセットする
  *   - strong要素を使って「説明文」を強調する
  */
 
+const pElement = document.getElementById('descripsion');
+pElement.innerHTML = 'JSで変更した後の<strong>説明文</strong>';
 
  /**
   * 課題3: img要素でid属性値が 'profile' のsrc属性値を変更する
@@ -19,6 +21,8 @@
   *     - 自分で用意する
   *     - 「https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg」を使う
   */
+const profileElement = document.getElementById('profile');
+profileElement.setAttribute('src', 'https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg');
 
 
 /**
@@ -26,4 +30,4 @@
  *   - セットするクラス属性値はstyle.cssの中身を参考にする
  *     - 背景色がh1要素の背景色が黄色になればOKです
  */
-
+titleElement.classList.add('bg-yellow');
