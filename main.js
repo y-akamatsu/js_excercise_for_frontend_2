e9e828fa5be1643e16c15b41ddeda339615a1cc9
y-akamatsu <ak1985tk@gmail.com>
2018-12-31 22:03:48 +0900

    初期化

--- main.js
+++ main.js
@@ -3,16 +3,14 @@
  *   - textContentを使う
  *     - 「JSで変更した後のタイトル」というテキストをセットする
  */
-const titleElement = document.getElementById('title');
-titleElement.textContent = 'JSで変更した後のタイトル';
+
 /**
  * 課題2: p要素でid属性値が `descripsion` のテキスト内容を変更する
  *   - innerHTMLを使う
  *     - 「JSで変更した後の説明文」というテキストをセットする
  *   - strong要素を使って「説明文」を強調する
  */
-const descripsionElement = document.getElementById('descripsion');
-descripsionElement.innerHTML ='JSで変更した後の<strong>説明文</strong>';
+
 
  /**
   * 課題3: img要素でid属性値が 'profile' のsrc属性値を変更する
@@ -21,8 +19,6 @@ descripsionElement.innerHTML ='JSで変更した後の<strong>説明文</strong>
   *     - 「https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg」を使う
   */
 
-const imageElement = document.getElementById('profile');
-imageElement.setAttribute('src', 'https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg');
 
 
 /**
@@ -30,4 +26,3 @@ imageElement.setAttribute('src', 'https://www.pakutaso.com/shared/img/thumb/TOMn
  *   - セットするクラス属性値はstyle.cssの中身を参考にする
  *     - 背景色がh1要素の背景色が黄色になればOKです
  */
-titleElement.className ='bg-yellow';
